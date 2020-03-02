25c1a8db792df586658bf4331336e5b7b9b362da
stephen <stephenmacgregor9@gmail.com>
2020-03-02 20:14:31 +0000

    updates to veiws/html

--- affiliate/views.py
+++ affiliate/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, FormView 
 from .forms import ContactForm, SubscribeForm
 from blog.models import Post
 
